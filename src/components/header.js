84d254222392e9ef66cc1c886995ba6d9ccc594d
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-14 15:49:17 +0200

    007 Adding some style

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import logo from '../img/castle.svg'
 
 class Header extends Component {
 	render() {
 		return (
 			<header>
-				<div>Riopar Studio</div>
+				<div>
+					<a href='#'>
+						<img src={logo} className='App-logo' alt='logo' />
+					</a>
+
+					<h1 className='logo'>
+						Riopar <br /> Studio
+					</h1>
+				</div>
 				<input />
 			</header>
 		)
