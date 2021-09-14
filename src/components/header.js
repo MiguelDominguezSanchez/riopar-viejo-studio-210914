1192a95d9f3e14c32aeabaa2ffe536321160a6f2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-09-14 13:25:51 +0200

    006 Types of components

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Header = () => {
-	const getTheYear = () => {
-		const newDate = new Date()
-		return newDate.getFullYear()
+class Header extends Component {
+	render() {
+		return (
+			<header>
+				<div>Riopar Studio</div>
+				<input />
+			</header>
+		)
 	}
-
-	return <div>The date is {5 + 5}</div>
 }
 
 export default Header
